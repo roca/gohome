4871d7f86288c323dae5b1c6072f6ab2d1ff9fdd
romel.campbell <romel.campbell@regeneron.com>
2025-01-16 10:15:18 -0500

    Chapter 2 REST API page 18

--- restapi/main.go
+++ restapi/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+type cmdresult struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+func homepage(write http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(write, "Go Home Simple REST API server")
+}
+
+func getdate(write http.ResponseWriter, _ *http.Request) {
+	result := cmdresult{}
+
+	out, err := exec.Command("date").Output()
+	if err == nil {
+		result.Success = true
+		result.Message = "The date is " + string(out)
+	}
+
+	json.NewEncoder(write).Encode(result)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/api/v1/getdate", getdate)
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		fmt.Println("Failed to start the server:", err)
+		os.Exit(1)
+	}
 }
