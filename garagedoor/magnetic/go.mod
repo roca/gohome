a6c91025c6e24700b3cd9212ec1c19a4dcbcba4a
romel.campbell <romel.campbell@regeneron.com>
2025-01-28 13:38:51 -0500

    garagedoor/magnetic

---
+++ garagedoor/magnetic/go.mod
@@ -0,0 +1,5 @@
+module magnetic
+
+go 1.23.4
+
+require github.com/stianeikeland/go-rpio/v4 v4.6.0
---
+++ garagedoor/magnetic/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+const pinNumber = 12
+
+type state rpio.state
+
+func (s state) String() string {
+	if s == state(rpio.Low) {
+		return "Open"
+	}
+	return "Closed"
+}
+
+func main() {
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Unmap gpio memory when done
+	defer rpio.Close()
+
+	pin := rpio.Pin(pinNumber)
+
+	pin.Input()
+	rpio.PullMode(pin, rpio.PullUp)
+	door := state(pin.Read())
+
+	fmt.Println("Door is:", door)
+}
