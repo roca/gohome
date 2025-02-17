38bd8ec8f3fa1f150af0c0f3890de382517fb361
roca <romelcampbell@gmail.com>
2025-02-17 14:07:33 -0500

    Getting weather for openweather api

--- lightingweather/openweathertest.go
+++ lightingweather/openweathertest.go
@@ -1,11 +1,32 @@
- package main
+package  main
 
- func main() {
-	w, err := owm.NewCurrent("F","EN",os.Getenv("OWM_API_KEY"))
+import (
+	"fmt"
+	"log"
+	"os"
+
+	owm "github.com/briandowns/openweathermap"
+)
+
+func main() {
+	w, err := owm.NewCurrent("F", "EN", os.Getenv("OWM_API_KEY"))
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	w.CurrentByZip(os.Getenv("ZIP_CODE"), os.Getenv("COUNTRY_CODE"))
-	fmt.Println(w.Main.Temp)
- }
+	w.CurrentByZipcode(os.Getenv("ZIP_CODE"), os.Getenv("COUNTRY_CODE"))
+	var currentTemp = w.Main.Temp
+
+	switch {
+	case currentTemp < 51:
+		fmt.Println("Blue")
+	case currentTemp >= 51 && currentTemp < 66:
+		fmt.Println("Yellow")
+	case currentTemp >= 66 && currentTemp < 80:
+		fmt.Println("Green")
+	case currentTemp >= 80 && currentTemp < 90:
+		fmt.Println("Orange")
+	case currentTemp >= 90:
+		fmt.Println("Red")
+	}
+}
