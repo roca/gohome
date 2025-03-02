e8ff8512eed1624cd6211ea397c299f909eb410c
Romel Campbell <RomelCampbell@gmail.com>
2025-03-02 04:32:39 -0500

    Birdwatcher motion sensor code

---
+++ birdwatcher/go.mod
@@ -0,0 +1,5 @@
+module birdwatcher
+
+go 1.23.6
+
+require github.com/stianeikeland/go-rpio/v4 v4.6.0
---
+++ birdwatcher/motion/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+/*
+GPIO pin 18 on the Pi Zero W is used to detect motion.
+The pin is connected to a PIR sensor.
+We will print a message to the console when motion is detected.
+*/
+
+func main() {
+	pin := rpio.Pin(18)
+
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer rpio.Close()
+
+	pin.Input()
+	pin.PullUp()
+	pin.Detect(rpio.RiseEdge)
+
+	fmt.Println("Sensing Enabled.")
+
+	for range time.Tick(500 * time.Millisecond) {
+		if pin.EdgeDetected() {
+			fmt.Println("Motion Detected!")
+		}
+	}
+}
