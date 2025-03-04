626f4bba42f0f7c7eee066d81c573a64f838d585
romel.campbell <romel.campbell@regeneron.com>
2025-03-04 13:44:38 -0500

    Adds correct file size

--- birdwatcher/imagepost/file-upload-example.go
+++ birdwatcher/imagepost/file-upload-example.go
@@ -12,20 +12,27 @@ func main() {
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	imagePath := "./sworks.jpeg"
 
+	file, err := os.Open(imagePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fileInfo, _ := file.Stat()
+	fileSize := fileInfo.Size()
+
 	params := slack.UploadFileV2Parameters{
 		// Channel: os.Getenv("CHANNEL_ID"),
 		// Channel: "general",
 		File:     imagePath,
 		Title:    "Sworks",
 		Filename: "sworks.jpeg",
-		FileSize: 303178,
+		FileSize: int(fileSize),
 	}
 
-	file,err := api.UploadFileV2(params)
+	uploadedFileInfo, err := api.UploadFileV2(params)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println("File ID:", file.ID)
+	fmt.Println("File ID:", uploadedFileInfo.ID)
 
 }
